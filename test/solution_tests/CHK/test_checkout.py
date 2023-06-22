5a11be05d01f7011bdfee6a2cfd26d6d52cdd08f
sbx_user1051 <sbx_user1051@169.254.177.221>
2023-06-22 15:10:41 +0000

    Thu Jun 22 15:10:41 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,6 +1,9 @@
 from solutions.SUM import sum_solution
+from solutions.CHK import checkout
 
 
 class TestSum():
-    def test_sum(self):
+
+    def test_item_a(self):
         assert sum_solution.compute(1, 2) == 3
+
